2bcf07ea89afcb924af5695f01481ca6b14285e8
seerosecode <keshalakemusic@gmail.com>
2019-06-13 14:47:38 -0400

    COMPLETE

--- js/index.js
+++ js/index.js
@@ -1,20 +1,25 @@
-/////////////////////////////VARIABLES///////////////////////////
+/////////////////////////////VARIABLES/////////////////////////
 const baseURL = "http://localhost:3000/monsters"//WORKING
 const formDiv = document.querySelector("#create-monster")//WORKING
 const monsterDiv = document.querySelector("#monster-container")//WORKING
 const backButton = document.querySelector("#back")//WORKING
 const nextButton = document.querySelector("#forward")//WORKING
-////////////////////////////FUNCTIONS///////////////////////////
+let page = document.querySelector("#page-number")
+let num = 1
+////////////////////////////FUNCTIONS//////////////////////////
 document.addEventListener("DOMContentLoaded", init)//WORKING
 
+//init WORKING
 function init(){
   console.log("LET'S GET IT STARTED UH HUH!")
 
   addForm()
   releaseTheMonsters()
 
-  nextButton.addEventListener("click", page2)
+  nextButton.addEventListener("click", nextPage)
   backButton.addEventListener("click", previousPage)
+
+
 }//END
 
 
@@ -25,17 +30,38 @@ function addForm(){
   //should add the create Monster form to the formDiv
   const form = document.createElement('form')
   const input = document.createElement('input')
-  form.action = baseURL
-  form.method = "POST"
   form.innerHTML =`
   name: <input type="text" name="name">
-  age: <input type="text" name="name">
+  age: <input type="text" name="age">
   description: <input type="text" name="description">
   <input type="submit" value="Submit">
   `
   formDiv.appendChild(form)
-  // form.addEventListener("submit", releaseTheMonsters())
+  formDiv.addEventListener("submit", newMonster)
 }//END
+
+//
+function newMonster(e){
+  e.preventDefault()
+  // console.log("Last step Queen! Finish Strong!")
+
+  const monster = {
+    name: e.target.name.value,
+    age: e.target.age.value,
+    description: e.target.age.value
+  }
+
+  fetch(baseURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      accepts: "application/json"
+    },
+    body: JSON.stringify(monster)
+  })
+  alert("Your monster has been created!")
+}//END
+
 
 
 //releaseTheMonsters WORKING
@@ -44,13 +70,14 @@ function releaseTheMonsters(){
   ///should release/FETCH the first 50 monsters from baseURL
   //should invoke the createMonsterHabitat function
   monsterDiv.innerHTML = ""
-  fetch(baseURL)
+  fetch(`${baseURL}/?_limit=50&_page=${num}`)
   .then(res => res.json())
-  .then(res => res.slice(0, 50).forEach(createMonsterHabitat))
+  .then(res => res.forEach(createMonsterHabitat))
+  page.innerText = num
 }//END
 
 
-
+//createMonsterHabit WORKING
 function createMonsterHabitat(monster){
   console.log("Don't worry about the time, you're doing good!")
   //should add the monster to the monsterDiv
@@ -62,54 +89,43 @@ function createMonsterHabitat(monster){
   monsterDiv.appendChild(newDiv)
 }//END
 
-function page2(){
+
+//nextPage WORKING
+function nextPage(){
   //should replace the 50 monsters shown on the page with the next 50 from baseURL
-  //should have the next 50 on cue if the next button is pressed
-  //should have the first 50 on cue if the back button is pressed
   // alert("YOU DA FUKKIN BEST!")
+  num += 1
   monsterDiv.innerHTML = ""
-  fetch(baseURL)
+  fetch(`${baseURL}/?_limit=50&_page=${num}`)
   .then(res => res.json())
-  .then(res => res.slice(50, 101).forEach(createMonsterHabitat))
-  nextButton.setAttribute("id", "2")
-  nextButton.addEventListener("click", page3)
+  .then(res => res.forEach(createMonsterHabitat))
+  nextButton.setAttribute("id", `${num}`)
+  page.innerText = num
 }//END
 
+
+
+//previousPage WORKING
 function previousPage(){
   //should load the previous 50 monsters onto the page
-  if (nextButton.id == "2"){
+  //alert("DA BES I EVA HAD!")
+  if (nextButton.id == "forward")
+    alert("where you goin dawg? page zero? smh")
+  else if (nextButton.id == "2"){
+    num -= 1
     monsterDiv.innerHTML = ""
     nextButton.id = "forward"
     releaseTheMonsters()
+    page.innerText = num
   }
-  else if (nextButton.id == "3"){
+  else if (nextButton.id > 2){
+    num -= 1
+    // debugger
     monsterDiv.innerHTML = ""
-    page2()
-  }
-  else if (nextButton.id == "4"){
-    monsterDiv.innerHTML = ""
-    page3()
-  }
-  else if (nextButton.id == null){
-    monsterDiv.innerHTML = ""
-    releaseTheMonsters()
+    fetch(`${baseURL}/?_limit=50&_page=${num}`)
+    .then(res => res.json())
+    .then(res => res.forEach(createMonsterHabitat))
+    nextButton.id = `${num}`
+    page.innerText = num
   }
 }//END
-
-function page3(){
-  monsterDiv.innerHTML = ""
-  fetch(baseURL)
-  .then(res => res.json())
-  .then(res => res.slice(101, 151).forEach(createMonsterHabitat))
-  nextButton.id = "3"
-  nextButton.addEventListener("click", page4)
-}//END
-
-function page4(){
-  monsterDiv.innerHTML = ""
-  fetch(baseURL)
-  .then(res => res.json())
-  .then(res => res.slice(152, 202).forEach(createMonsterHabitat))
-  nextButton.id = "4"
-  nextButton.addEventListener("click", alert("THAT'S ALL FOLKS!"))
-}
